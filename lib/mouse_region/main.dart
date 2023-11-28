6ace1322a3d4966704ccc56b0e628bcbb09cf844
phamnhantucode <bao.trancong.2003@gmail.com>
2023-11-28 09:04:43 +0700

    flutter MouseRegion widget

---
+++ lib/mouse_region/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double x = 0.0;
+  double y = 0.0;
+
+  void _updatePosition(PointerHoverEvent event) {
+    setState(() {
+      x = event.position.dx;
+      y = event.position.dy;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onHover: _updatePosition,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+        ),
+        body: Center(
+          child: ColoredBox(
+            color: Colors.blue.shade100,
+            
+            child: Text('Current position of mouse: x = $x, y = $y'),
+          ),
+        ),
+      ),
+    );
+  }
+}
